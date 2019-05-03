ae8c72a75e9a9a2d7e9b3fbdb323e58f227d80ea
Mujeeb Ishaque <mujeeb.ishaque@gmail.com>
2019-05-03 21:20:36 +0500

    Add files via upload

---
+++ docsummary/business_logic/manage.py
@@ -0,0 +1,26 @@
+import variables
+variables.initialize()
+import pdf2txt
+import extractor
+import pdf_downloader
+#from pdf_downloader import download_count
+import os
+
+# import argparse
+
+# parser = argparse.ArgumentParser(description='Description of your program')
+# parser.add_argument('-a','--add', help='Add a pdf file to convert to text', required=True)
+# args = vars(parser.parse_args())
+
+# if args['add']:
+#     documentName = args['add']
+
+os.system('python pdf2txt.py ' + documentName)
+
+#extractor.extrac()
+#pdf_downloader.download()
+
+
+        
+                
+   
